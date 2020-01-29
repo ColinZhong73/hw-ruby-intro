9866744d12f3d7f9a9284d1764299eac59b0179f
EC2 Default User <ec2-user@ip-172-31-37-144.ec2.internal>
2020-01-29 00:39:26 +0000

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,42 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr.empty?
+    0
+  else
+    num = 0
+    arr.each { |x| num += x}
+    num
+  end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  max_2_arr = arr.max(2)
+  sum(max_2_arr)
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  isSummed = false
+  if !arr.empty?
+    arr.sort!
+    arr.each{
+    |x| num = n - x
+    if num >= 0
+      narr = arr.select{|y| y >= x}
+      narr.shift
+      if !narr.empty?
+        narr.each { |y| 
+        if num == y
+          isSummed = true
+        end
+        }
+      end
+    end
+    }
+  end
+  isSummed
 end
 
 # Part 2
